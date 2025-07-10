d0261af53c2017ace962e88c10de0e93c48161c1
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-10 08:55:53 -0300

    fix: dead player hide

--- player_windows.gd
+++ player_windows.gd
@@ -7,19 +7,26 @@ var active_index: int = -1
 func _ready() -> void:
 	for i in range(get_child_count()):
 		if i < party.size():
-			get_child(i).data = party[i]
+			var child = get_child(i)
+			child.data = party[i]
+			party[i].hp_changed.connect(_on_player_hp_changed.bind(i, child)) # conecta hp_changed
 			print("player found")
 		else:
 			get_child(i).data = null
 
 func activate(player_index: int) -> void:
-	if active_index == player_index:
-		return
-	
-	if active_index != -1:
+	# desativa anterior
+	if active_index != -1 and active_index < get_child_count():
 		get_child(active_index).activate(false)
-	
+
 	active_index = player_index
-	
-	if active_index != -1:
-		get_child(active_index).activate(true)
+
+	# ativa novo, se existir e estiver vivo
+	if active_index != -1 and active_index < get_child_count():
+		var child = get_child(active_index)
+		if child.visible: # só ativa se o player ainda está visível (vivo)
+			child.activate(true)
+
+func _on_player_hp_changed(hp: int, hp_max: int, value_change: int, index: int, window: Control) -> void:
+	if hp <= 0:
+		window.hide()
