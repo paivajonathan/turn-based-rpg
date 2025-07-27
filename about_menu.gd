7a14478fc9008092b12393d506efdaff8f10a164
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-27 17:48:03 -0300

    feat: added button click and hover

--- about_menu.gd
+++ about_menu.gd
@@ -1,6 +1,9 @@
 extends TextureRect
 
 @onready var animation_menu : AnimationPlayer = $AnimationMenu
+@onready var sfx_click: AudioStreamPlayer = $"../../sfx_click"
+@onready var sfx_hover: AudioStreamPlayer = $"../../sfx_hover"
+
 
 func _ready() -> void:
 	start_loop()
@@ -9,4 +12,9 @@ func start_loop() -> void:
 	animation_menu.play("loop")
 
 func _on_button_voltar_pressed() -> void:
+	sfx_click.play(0.05)
+	await sfx_click.finished
 	get_tree().change_scene_to_file("res://menu_helel.tscn")
+
+func _on_button_voltar_mouse_entered() -> void:
+	sfx_hover.play(0.11)
--- menu_helel.gd
+++ menu_helel.gd
@@ -1,6 +1,8 @@
 extends TextureRect
 
 @onready var animation_menu : AnimationPlayer = $AnimationMenu
+@onready var sfx_click: AudioStreamPlayer = $"../../sfx_click"
+@onready var sfx_hover: AudioStreamPlayer = $"../../sfx_hover"
 
 func _ready() -> void:
 	start_loop()
@@ -9,10 +11,24 @@ func start_loop() -> void:
 	animation_menu.play("loop")
 
 func _on_button_jogar_pressed() -> void:
+	sfx_click.play(0.05)
 	TransitionHandler.fade_out(get_tree().current_scene, "res://gamemap.tscn", .8, Color.ORANGE)
 
 func _on_button_sobre_pressed() -> void:
+	sfx_click.play(0.05)
+	await sfx_click.finished
 	get_tree().change_scene_to_file("res://menu_about.tscn")
 
 func _on_button_sair_pressed() -> void:
+	sfx_click.play(0.05)
+	await sfx_click.finished
 	get_tree().quit()
+
+func _on_button_jogar_mouse_entered() -> void:
+	sfx_hover.play(0.11)
+
+func _on_button_sobre_mouse_entered() -> void:
+	sfx_hover.play(0.11)
+
+func _on_button_sair_mouse_entered() -> void:
+	sfx_hover.play(0.11)
