ac1c6fc184c3f56e2313494ebb20da167aee7e1e
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-29 16:19:29 -0300

    update: setting visible label

--- Scenes/interaction.gd
+++ Scenes/interaction.gd
@@ -3,10 +3,11 @@ extends Area2D
 var can_interact = false:
 	set(value):
 		can_interact = value
+		%Label.visible = value
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	can_interact = true
 
 
 func _on_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	can_interact = false
