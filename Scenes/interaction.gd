197adbaa4e3410f2790634546d19e91ae6289dbc
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-29 16:17:22 -0300

    setting base ui interaction

---
+++ Scenes/interaction.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var can_interact = false:
+	set(value):
+		can_interact = value
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
