38a88c619eeb741592e0fb1b4227ec402926beb4
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-30 10:19:22 -0300

    update: adding sfx collision enemy in tileset

--- Scripts/enemy_tilemap.gd
+++ Scripts/enemy_tilemap.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
 @export var lamento_scene: String = "res://Scenes/lamento_drafting.tscn"
+@onready var collide_sfx = preload("res://music/sfx/angel-tilemap-collision.mp3")
+@onready var player = AudioStreamPlayer.new()
 
 func _ready():
+	player.stream = collide_sfx
+	add_child(player)
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body: Node) -> void:
@@ -10,4 +14,5 @@ func _on_body_entered(body: Node) -> void:
 
 	if body.name == "Player":
 		print("Colidiu com Player, carregando cena de batalha…")
-		TransitionHandler.fade_out(get_tree().current_scene, lamento_scene, .8, Color.BLACK)
+		player.play()
+		TransitionHandler.fade_out(get_tree().current_scene, lamento_scene, 2, Color.BLACK)
