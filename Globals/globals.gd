caeda6268824bb9a2080f383cc64be3a397e921f
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-30 08:05:47 -0300

    feat: added npc_treshold as global

--- Globals/globals.gd
+++ Globals/globals.gd
@@ -1,6 +1,7 @@
 # Globals.gd
 extends Node
 
+var npc_threshold: int = 0
 var log_func = null
 
 func log_message(message: String) -> void:
--- lamento_drafting.gd
+++ lamento_drafting.gd
@@ -1,1 +1,23 @@
 extends CanvasLayer
+
+@onready var label = $Label
+@onready var button = $Button
+
+var sorte_gerada = false
+
+func _ready():
+	button.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+	if not sorte_gerada:
+		var rng = RandomNumberGenerator.new()
+		rng.randomize()
+		Globals.npc_threshold = rng.randi_range(1, 20)
+
+		# Atualiza label e botão
+		label.text = "Sua resistência será: %d" % Globals.npc_threshold
+		button.text = "CONTINUAR"
+		sorte_gerada = true
+	else:
+		# Troca para cena da batalha
+		get_tree().change_scene_to_file("res://battle.tscn")
