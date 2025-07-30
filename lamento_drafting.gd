0919bd6d866dda2222132085a3669513731a50ef
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-30 09:00:02 -0300

    update: styling label text

--- lamento_drafting.gd
+++ lamento_drafting.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 @onready var label = $Label
 @onready var button = $Button
+@onready var spr_anjo = $SprLamento
 
 var sorte_gerada = false
 
@@ -13,11 +14,17 @@ func _on_button_pressed():
 		var rng = RandomNumberGenerator.new()
 		rng.randomize()
 		Globals.npc_threshold = rng.randi_range(1, 20)
-
+		var helel_font = load("res://font/Metropolitan.ttf")
+		var helel_theme = load("res://font/hele_theme.tres")
+		
 		# Atualiza label e botão
+		label.add_theme_font_override("font",helel_font)
+		label.theme = helel_theme
 		label.text = "Sua resistência será: %d" % Globals.npc_threshold
 		button.text = "CONTINUAR"
 		sorte_gerada = true
+		
+		spr_anjo.texture = load("res://Art/anjo_sprite/spr_lamento1.png")
 	else:
 		# Troca para cena da batalha
-		get_tree().change_scene_to_file("res://Scenes/battle.tscn")
+		TransitionHandler.fade_out(get_tree().current_scene, "res://Scenes/battle.tscn", .8, Color.FIREBRICK)
